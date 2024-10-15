ff93e941f04771d06b012dd69e93271306fad25b
Nitish <tnitish654@gmail.com>
2024-10-15 20:49:12 +0800

    ✨ Added hero text and text scrolling animation 🐞 Fixed Poppins font import

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -8,7 +8,24 @@ export default {
             sans: ["DM Sans Variable", ...defaultTheme.fontFamily.sans],
             display: ["Poppins", "sans-serif"],
         },
-        extend: {},
+        extend: {
+            animation: {
+                infiniteScroll: "scrollUp 4s infinite",
+            },
+            keyframes: {
+                scrollUp: {
+                    "16.5%, 33%": {
+                        transform: "translateY(-25%)",
+                    },
+                    "49.5%, 66%": {
+                        transform: "translateY(-50%)",
+                    },
+					"82.5%, 100%": {
+						transform: "translateY(-75%)",
+					},
+                },
+            },
+        },
     },
     plugins: [],
 }
